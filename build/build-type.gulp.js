6094eece7e18f63a0f9bcb7dc79ab80014b2cc5f
焦质晔 <jiaozhiye@163.com>
2021-02-15 14:17:22 +0800

    更新了架构代码

---
+++ build/build-type.gulp.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: 焦质晔
+ * @Date: 2021-02-15 10:50:25
+ * @Last Modified by: 焦质晔
+ * @Last Modified time: 2021-02-15 14:07:55
+ */
+'use strict';
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { series, src, dest } = require('gulp');
+const ts = require('gulp-typescript');
+const ignore = require('gulp-ignore');
+const rename = require('gulp-rename');
+const tsProject = ts.createProject('../tsconfig.json');
+const utils = require('./utils');
+
+const LIB_NAME = 'qm-design';
+
+function compile() {
+  const tsResult = tsProject
+    .src()
+    .pipe(ignore.include(['packages/**/*', 'typings/vue-shim.d.ts']))
+    .pipe(tsProject());
+  return tsResult.dts.pipe(dest(utils.resolve('lib')));
+}
+
+function copydts() {
+  return src(utils.resolve('typings/vue-shim.d.ts'))
+    .pipe(rename(`${LIB_NAME}.d.ts`))
+    .pipe(dest(utils.resolve('lib')));
+}
+
+// gulp.task('default', function () {
+//   const tsResult = tsProject
+//     .src()
+//     .pipe(ignore.include(['packages/**/*', 'typings/vue-shim.d.ts']))
+//     .pipe(tsProject());
+//   return merge([
+//     tsResult.js.pipe(gulp.dest(utils.resolve('lib'))),
+//     tsResult.dts.pipe(gulp.dest(utils.resolve('lib'))),
+//   ]);
+// });
+
+exports.build = series(compile, copydts);
--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -2,14 +2,26 @@
  * @Author: 焦质晔
  * @Date: 2021-02-09 09:03:59
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-02-11 16:03:37
+ * @Last Modified time: 2021-02-15 12:07:31
  */
 import { defineComponent } from 'vue';
+
+import { sleep } from '../../_utils/util';
 
 export default defineComponent({
   name: 'QmButton',
   componentName: 'QmButton',
+  methods: {
+    async handleClick() {
+      await sleep(1000);
+      console.log(1234);
+    },
+  },
   render() {
-    return <el-button icon="el-icon-delete">按钮</el-button>;
+    return (
+      <el-button onClick={this.handleClick} icon="el-icon-delete">
+        按钮
+      </el-button>
+    );
   },
 });
