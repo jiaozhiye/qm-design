f8d61fe2a49e5d8674226aa51944c514773d0630
焦质晔 <jiaozhiye@163.com>
2021-02-16 18:15:59 +0800

    更新了架构代码

--- build/build-type.rollup.js
+++ build/build-type.rollup.js
@@ -2,17 +2,22 @@
  * @Author: 焦质晔
  * @Date: 2021-02-12 09:22:19
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-02-12 11:45:55
+ * @Last Modified time: 2021-02-16 17:39:35
  */
 'use strict';
 
-import vuePlugin from 'rollup-plugin-vue';
 import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import vuePlugin from 'rollup-plugin-vue';
 import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import jsx from 'acorn-jsx';
 import pkg from '../package.json';
-const deps = Object.keys(pkg.dependencies);
 const utils = require('./utils');
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const deps = Object.keys(pkg.dependencies);
 
 export default [
   {
@@ -34,9 +39,12 @@ export default [
           include: ['packages/**/*', 'typings/vue-shim.d.ts'],
           exclude: ['node_modules'],
         },
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        abortOnError: false,
       }),
+      commonjs({ extensions }),
+      babel({ babelHelpers: 'runtime', extensions }),
     ],
+    acornInjectPlugins: [jsx()],
     external(id) {
       return /^vue/.test(id) || deps.some((k) => new RegExp('^' + k).test(id));
     },
