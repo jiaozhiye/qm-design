346bc54b21878265cb9a7328b6dd1bb10dee948c
焦质晔 <jiaozhiye@163.com>
2021-03-17 12:20:18 +0800

    在线文档2

---
+++ website/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>asdasd</div>
+</template>
+
+<script lang="tsx">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style lang="scss"></style>
