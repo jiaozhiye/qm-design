73de4b3046e0d041d3d69ea35df18821098e27b4
焦质晔 <jiaozhiye@163.com>
2021-05-12 22:45:49 +0800

    更新了架构代码

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 // Preset ordering is reversed, so `@babel/typescript` will called first
 // Do not put `@babel/typescript` before `@babel/env`, otherwise will cause a compile error
 // See https://github.com/babel/babel/issues/12066
+const presets = [['@babel/preset-env', { loose: true, modules: false }], '@babel/preset-typescript'];
 const plugins = ['@vue/babel-plugin-jsx', '@babel/plugin-proposal-class-properties'];
 
 module.exports = {
@@ -8,6 +9,10 @@ module.exports = {
     lib: {
       presets: [],
       plugins,
+    },
+    utils: {
+      presets,
+      plugins: [],
     },
     web: {
       presets: [['@babel/preset-env', { modules: false, corejs: 3, useBuiltIns: 'usage' }]],
