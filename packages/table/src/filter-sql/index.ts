009b610cca670e1204e76cd103ccff4aa0231fdf
焦质晔 <jiaozhiye@163.com>
2021-05-25 14:35:41 +0800

    优化了基础组件

--- packages/table/src/filter-sql/index.ts
+++ packages/table/src/filter-sql/index.ts
@@ -2,17 +2,18 @@
  * @Author: 焦质晔
  * @Date: 2020-07-11 10:51:46
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-05-07 17:40:38
+ * @Last Modified time: 2021-05-25 14:22:43
  */
 import sf from './lib/filter_string';
 import vr from './lib/variables_replacement';
 import { matchWhere } from './lib/operations';
+import { IRecord } from '../table/types';
 
-const _query = (...rest) => {
+function _query(...rest: any[]): boolean {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   return matchWhere(...rest);
-};
+}
 
 export const stringify = sf.stringify;
 
@@ -20,15 +21,15 @@ export const array_format = sf.array_format;
 
 export const isBracketBalance = vr.isBracketBalance;
 
-export const where = (array, query) => {
-  const result: string[] = [];
+export const where = <T extends IRecord>(array: T[], query: string): T[] => {
+  const result: T[] = [];
 
-  // Replace AND and OR to && and ||
+  // replace AND, OR to &&, ||
   query = sf.replace_symbols(query);
   query = vr.replace_variables(query, 'array[i]');
   // console.log(`Conditionals`, query);
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0, len = array.length; i < len; i++) {
     if (eval(query)) {
       result.push(array[i]);
     }
