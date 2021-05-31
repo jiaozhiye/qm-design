d3042be96f8d6d36770fbc55976f7f091683faeb
焦质晔 <jiaozhiye@163.com>
2021-05-31 09:54:41 +0800

    优化了form组件

--- packages/_utils/resize-event.ts
+++ packages/_utils/resize-event.ts
@@ -2,29 +2,27 @@
  * @Author: 焦质晔
  * @Date: 2021-02-08 19:28:25
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-02-14 14:28:17
+ * @Last Modified time: 2021-05-31 09:23:42
  */
 import ResizeObserver from 'resize-observer-polyfill';
 import isServer from './isServer';
-import { CustomHTMLElement } from './types';
+import { CustomHTMLElement, AnyFunction } from './types';
 
 export type ResizableElement = CustomHTMLElement<{
-  __resizeListeners__: Array<(...args: unknown[]) => unknown>;
+  __resizeListeners__: Array<AnyFunction<void>>;
   __ro__: ResizeObserver;
 }>;
 
-const resizeHandler = function (entries: ResizeObserverEntry[]) {
+const resizeHandler = function (entries: ResizeObserverEntry[]): void {
   for (const entry of entries) {
     const listeners = (entry.target as ResizableElement).__resizeListeners__ || [];
     if (listeners.length) {
-      listeners.forEach((fn) => {
-        fn();
-      });
+      listeners.forEach((fn) => fn());
     }
   }
 };
 
-export const addResizeListener = function (element: ResizableElement, fn: (...args: unknown[]) => unknown): void {
+export const addResizeListener = function (element: ResizableElement, fn: AnyFunction<void>): void {
   if (isServer || !element) return;
   if (!element.__resizeListeners__) {
     element.__resizeListeners__ = [];
@@ -34,7 +32,7 @@ export const addResizeListener = function (element: ResizableElement, fn: (...ar
   element.__resizeListeners__.push(fn);
 };
 
-export const removeResizeListener = function (element: ResizableElement, fn: (...args: unknown[]) => unknown): void {
+export const removeResizeListener = function (element: ResizableElement, fn: AnyFunction<void>): void {
   if (!element || !element.__resizeListeners__) return;
   element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1);
   if (!element.__resizeListeners__.length) {
--- packages/form/src/form-cols-mixin.ts
+++ packages/form/src/form-cols-mixin.ts
@@ -2,10 +2,12 @@
  * @Author: 焦质晔
  * @Date: 2020-06-01 13:23:53
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-02-24 16:02:49
+ * @Last Modified time: 2021-05-31 09:48:28
  */
 import { addResizeListener, removeResizeListener } from '../../_utils/resize-event';
 import { debounce } from '../../_utils/util';
+
+const COL_WIDTH = 300;
 
 export const FormColsMixin = {
   data() {
@@ -21,10 +23,12 @@ export const FormColsMixin = {
   },
   methods: {
     resizeListener(): void {
-      const c = Math.floor(this.$refs[`form`].$el.offsetWidth / 300);
-      let cols = 24 % c === 0 ? c : c - 1;
-      cols = cols < 2 ? 2 : cols;
-      cols = cols > 6 ? 6 : cols;
+      const w: number = this.$refs[`form`].$el.offsetWidth;
+      if (w === 0) return;
+      let cols = Math.floor(w / COL_WIDTH);
+      cols = 24 % cols === 0 ? cols : cols - 1;
+      cols = cols < 1 ? 1 : cols;
+      cols = cols > 8 ? 8 : cols;
       this.flexCols = typeof this.cols === 'undefined' ? cols : this.cols;
     },
     bindResizeEvent(): void {
